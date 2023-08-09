716a7be6a69d022c7c4033ad7061f1ac8911ceb8
Blaziumm <eclipsium10@gmail.com>
2023-08-09 17:31:21 +0000

    Final Setup and Robot-config translation

---
+++ TranslatedSrc/robot-config (copy).py
@@ -0,0 +1,19 @@
+from vex import *
+import urandom
+
+# A global instance of brain used for printing to the V5 Brain screen
+brain=Brain()
+
+# VEXcode device constructors
+
+# VEXcode generated functions
+
+
+
+ #
+ # Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ # 
+ # This should be called at the start of your int main function.
+ #
+def vexcodeInit():
+  // nothing to initialize
